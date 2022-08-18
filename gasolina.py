0e1213b428ff529ee2066a26f21c3e342ad0fdff
maira-anelise-corteze <macorteze@gmail.com>
2022-08-18 13:28:53 +0000

    adicionando arquivos gasolina.csv gasolina.png gasolina.py

---
+++ gasolina.py
@@ -0,0 +1,15 @@
+# código de geração do gráfico 
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('gasolina.csv', sep=',')
+
+with sns.axes_style('whitegrid'):
+
+  grafico = sns.lineplot(data=df, x="dia", y="venda", palette="pastel")
+  grafico.set(title='Preço Gasolina por Dia', xlabel='Dia', ylabel='Preço');
+
+plt.savefig('gasolina.png')
+
+print('Sucesso')
